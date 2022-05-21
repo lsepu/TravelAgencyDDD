0cd047f266636e08bfd19428bd0a6725e0f98ebf
lsepu <55538804+lsepu@users.noreply.github.com>
2022-05-20 20:32:11 -0500

    Project initialized with dependencies

---
+++ src/main/java/com/sofkaU/TravelAgencyDDD/TravelAgencyDddApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.TravelAgencyDDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkaU/TravelAgencyDDD/TravelAgencyDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaU.TravelAgencyDDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
